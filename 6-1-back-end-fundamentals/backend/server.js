f2b5e164932406e56930891bd88ea9b5103ae522
Sadeem-Alghamdi <s202244580@kfupm.edu.sa>
2026-03-30 11:38:12 +0300

    1

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,25 +1,33 @@
 // TODO 1: Import Express
-
-
-
-// TODO 2: Create the Express app and store it in a variable named app
-
-
+const express = require("express");
 
 // TODO 3: Allow React to access the server
+const cors = require("cors");
 
+// TODO 2: Create the Express app
+const app = express();
 
+app.use(cors());
 
 // TODO 5: Create the home route "/"
-
-
+app.get("/", (req, res) => {
+    res.send("Hello from the back-end server");
+});
 
 // TODO 6: Create the "/about" route
-
-
+app.get("/about", (req, res) => {
+    res.send("This is the about route");
+});
 
 // TODO 7: Create the "/student" route
-
-
+app.get("/student", (req, res) => {
+    res.json({
+        name: "Sadeem",
+        major: "Software Engineering",
+    });
+});
 
 // TODO 4: Start the server on port 3000
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
